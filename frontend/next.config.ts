e0a760943f2eaccb8c7c4273f0105195450137c7
Zabih Keraam <165507052+Zabihkeraam1@users.noreply.github.com>
2025-05-14 17:28:13 +0430

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone', 
+  // output: 'standalone', 
   /* config options here */
+  output: 'export',
 };
 
 export default nextConfig;
